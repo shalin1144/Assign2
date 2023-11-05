fd0009bd84d9c4a9067c4dc4b1eb19f8d4808f43
shalin1144 <shalin.vaidya@ontariotechu.net>
2023-11-04 23:41:07 -0400

    A2Update

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', {
+    title: 'About'
+  });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', {
+    title: 'Projects'
+  });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', {
+    title: 'Contact Me'
+  });
+});
+module.exports = router;
